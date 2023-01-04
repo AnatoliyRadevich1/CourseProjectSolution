37bd010d6fa7d18b60104d452aa7342f4cf28399
ALMAZ <ALMAZ@ALMAZ>
2023-01-05 02:14:35 +0300

    Добавьте файлы проекта.

---
+++ CourseProject/Pages/AdminFolder/Index.cshtml.cs
@@ -0,0 +1,87 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace CourseProject.Pages.AdminFolder
+{
+    public class IndexModel : PageModel
+    {
+        public List<AdminInfo> myAdminInfo = new List<AdminInfo>();
+        public void OnGet()
+        {
+            try
+            {
+				string conectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\pc\Desktop\ITransition\CourseProject\CourseProjectSolution\DataBase\SampleDatabaseWalkthrough\CourseDB.mdf;Integrated Security=True";
+				using (SqlConnection connection = new SqlConnection(conectionString))
+				{ 
+					connection.Open();
+					string sql = "SELECT * FROM fulltable";
+					using (SqlCommand command = new SqlCommand(sql, connection))
+					{
+						using (SqlDataReader reader = command.ExecuteReader())
+						{
+							while (reader.Read())
+							{
+								AdminInfo adminInfo = new AdminInfo();
+								adminInfo.Id = reader.GetInt32(0);
+								adminInfo.Username = reader.GetString(1);
+								adminInfo.Userpass = reader.GetString(2);
+								adminInfo.User_social_network_VK_acceess = reader.GetString(3);
+								adminInfo.User_social_network_FB_acceess = reader.GetString(4);
+								adminInfo.User_social_network_TG_acceess = reader.GetString(5);
+								adminInfo.User_page_address = reader.GetString(6);
+								adminInfo.User_name_of_review = reader.GetString(7);
+								adminInfo.User_group_of_review_for_films = reader.GetBoolean(8);
+								adminInfo.User_group_of_review_for_books = reader.GetBoolean(9);
+								adminInfo.User_group_of_review_for_games = reader.GetBoolean(10);
+								adminInfo.User_tag  = reader.GetString(11);
+								adminInfo.User_review_text = reader.GetString(12);
+								adminInfo.User_icon_address  = reader.GetString(13);
+								adminInfo.User_marks = reader.GetByte(14);
+								adminInfo.AnyUser_rate = reader.GetByte(15);
+								adminInfo.Average_rate = reader.GetDecimal(16);
+								adminInfo.User_dark_theme  = reader.GetBoolean(17);
+								adminInfo.User_language_selection  = reader.GetString(18);
+								adminInfo.User_is_blocked = reader.GetBoolean(19);
+								adminInfo.User_is_deleted = reader.GetBoolean(20);
+								adminInfo.User_pdf_file_address  = reader.GetString(21);
+							}
+						}
+					}
+				}
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Необработанное исключение "+ex.ToString());
+            }
+        }
+    }
+    public class AdminInfo
+    {
+		public int Id { get; set; }
+		public string Username { get; set; }
+		public string Userpass { get; set; }
+		public string User_social_network_VK_acceess { get; set; }
+		public string User_social_network_FB_acceess { get; set; }
+		public string User_social_network_TG_acceess { get; set; }
+		public string User_page_address { get; set; }
+		public string User_name_of_review { get; set; }
+		public bool User_group_of_review_for_films { get; set; }
+		public bool User_group_of_review_for_books { get; set; }
+		public bool User_group_of_review_for_games { get; set; }
+		public string User_tag { get; set; }
+		public string User_review_text { get; set; }
+		public string User_icon_address { get; set; }
+		public byte User_marks { get; set; }
+		public byte AnyUser_rate { get; set; }
+		public decimal Average_rate { get; set; }
+		public bool User_dark_theme { get; set; }
+		public string User_language_selection { get; set; }
+		public bool User_is_blocked { get; set; }
+		public bool User_is_deleted { get; set; }
+		public string User_pdf_file_address { get; set; }
+
+	}
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,43 @@
+﻿CREATE TABLE fulltable (
+						id INT NOT NULL PRIMARY KEY IDENTITY,
+						username VARCHAR(30) NOT NULL,
+						userpass VARCHAR(30) NOT NULL,
+						user_social_network_VK_acceess VARCHAR (30),
+						user_social_network_FB_acceess VARCHAR (30),
+						user_social_network_TG_acceess VARCHAR (30),
+						user_page_address VARCHAR(30) NOT NULL,
+						user_name_of_review VARCHAR(30),
+						user_group_of_review_for_films BIT,
+						user_group_of_review_for_books BIT,
+						user_group_of_review_for_games BIT,
+						user_tag TEXT,
+						user_review_text TEXT,
+						user_icon_address VARCHAR(50),
+						user_marks TINYINT,
+						anyuser_rate TINYINT,
+						average_rate DECIMAL(2,1),
+						user_dark_theme BIT,
+						user_language_selection VARCHAR(30) NOT NULL,
+						user_is_blocked BIT,
+						user_is_deleted BIT,
+						user_pdf_file_address VARCHAR(50)
+						);
+
+
+
+INSERT INTO fulltable (username, userpass, user_social_network_VK_acceess, user_social_network_FB_acceess, user_social_network_TG_acceess)
+VALUES
+('Tom Sawyer', 'Tom Sawyer pass', 'vk.com/TS', 'facebook.com/TS', '@TS'),
+('Bob Wald', 'Bob Wald pass', 'vk.com/BW', 'facebook.com/BW', '@BW'),
+('John Snow', 'John Snow pass', 'vk.com/JS', 'facebook.com/JS', '@JS'),
+('Kate Summer', 'Kate Summer pass', 'vk.com/KS', 'facebook.com/KS', '@KS'),
+('Ann Gingerhair', 'Ann Gingerhair pass', 'vk.com/AG', 'facebook.com/AG', '@AG'),
+('Alma Berg', 'Alma Berg pass', 'vk.com/AB', 'facebook.com/AB', '@AB')
+
+GO
+SELECT user_marks FROM fulltable
+WHERE user_marks>=0 AND user_marks <=10;
+
+
+
+
